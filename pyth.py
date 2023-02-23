735311da92b11e2d90e41c830af844d395c0fd26
PrinceAndrew073 <princeandrew073@gmail.com>
2023-02-23 17:15:28 +0000

    first commit

---
+++ pyth.py
@@ -0,0 +1,15 @@
+first_name = ("Prince")
+last_name = ("Ahialey")
+phone_number = 233242853383
+age =55 
+
+print(first_name + last_name)
+print(phone_number)
+print(age)
+
+
+li_st = ["ella", "prince", "patrick"]
+print(li_st)
+print(li_st[2])
+li_st.append("bright")
+print(li_st)
